8d8483ec84f2ec65bb10ba6d7aae8dac9e539b68
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2020-07-29 15:13:45 +0300

    Сохранено 29 июля 2020 года

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header'
+import TodoItem from './components/todoitem'
+import AddTodo from './components/addTodo'
+// import Sandbox from './components/sandbox'
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    { text: 'Выпить кофе', key: '1' },
+    { text: 'Сделать данное приложение', key: '2' },
+    { text: 'Радоваться жизни', key: '3' },
+  ])
+
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key !== key)
+    })
+  }
+
+  const submitHandler = (text) => {
+
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('ОШИБКА!', 'Запланированное дело не может быть менее 3 символов.', [
+        { text: 'Понятно', onPress: () => console.log('alert closed') }
+      ])
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Sandbox />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.contant}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  contant: {
+    flex: 1,
+    padding: 40
+  },
+  list: {
+    flex: 1,
+    marginTop: 20
+  }
 });
---
+++ components/sandbox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default function Sandbox() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>one</Text>
+            <Text style={styles.boxTwo}>two</Text>
+            <Text style={styles.boxThree}>three</Text>
+            <Text style={styles.boxFour}>four</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#ddd'
+    }
+}); 
